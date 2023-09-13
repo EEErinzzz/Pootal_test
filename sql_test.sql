c060a9cd814f410907d1f19b408b6fae7e4bd911
EEErinzzz <116625261+EEErinzzz@users.noreply.github.com>
2023-09-13 23:57:59 +0800

    Create sql_test.sql
    finish test_3

---
+++ sql_test.sql
@@ -0,0 +1,16 @@
+SELECT DepartmentId, max( Salary ) 
+FROM Employee 
+GROUP BY DepartmentId 
+SELECT
+	Department.NAME AS Department,
+	Employee.NAME AS Employee,
+	Salary 
+FROM
+	Employee,
+	Department 
+WHERE
+	Employee.DepartmentId = Department.Id 
+	AND ( Employee.DepartmentId, Salary ) 
+    IN (SELECT DepartmentId, max( Salary ) 
+        FROM Employee 
+        GROUP BY DepartmentId )
